a08e9741053d35e83e496fd6c88e4e1e079f872c
Chris Hanagan <chris.hanagan@gmail.com>
2025-01-22 14:29:57 -0500

    fix PgTop to align the icons

--- src/components/PgTop.svelte
+++ src/components/PgTop.svelte
@@ -1,25 +1,24 @@
 <script>
     // import { Button, Col, Row } from "@sveltestrap/sveltestrap";
-    import { Grid, Row, Column } from "carbon-components-svelte";
+    // import { Grid, Row, Column } from "carbon-components-svelte";
+    import { Container, Row, Col } from "@sveltestrap/sveltestrap";
 
     let ihLogo = "./images/ih-logo.png";
     let cbLogo = "./images/cb-logo.png";
 
-
-
 </script>
 
 <main>
-    <Grid>
-        <Row>
-            <Column vertical-alighment="center">
+    <Container>
+        <Row class="align-items-center">
+            <Col xs="6">
                 <img src={ihLogo} alt="IH Logo" width="300" height="100" />
-            </Column>
-            <Column vertical-alighment="center">
+            </Col>
+            <Col xs="6">
                 <img src={cbLogo} alt="CB Logo" width="240" height="40" />
-            </Column>
+            </Col>
         </Row>
-    </Grid>
+    </Container>
 </main>
 
 
